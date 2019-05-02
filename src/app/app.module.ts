7b6d04ccf6f636b35566d1c1e8216c2d76eb046a
Asitha <asithaj@99x.lk>
2019-05-02 16:57:01 +0530

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // Sub Applications
-import { App1SharedModule } from '../../projects/sub-app1/src/app/app.module';
-import { App2SharedModule } from '../../projects/sub-app2/src/app/app.module';
+import { App1SharedModule } from './../../submodule1/sub-app1/src/app/app.module';
+import { App2SharedModule } from './../../submodule2/sub-app2/src/app/app.module';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
